b88026674dcab8c1f95a84f5507b6efb5fd111a0
Kenneth Felix <kennf03@gmail.com>
2022-04-14 11:38:58 -0400

    written the entities

--- src/main/java/com/bytesize/entities/Transaction.java
+++ src/main/java/com/bytesize/entities/Transaction.java
@@ -1,4 +1,96 @@
 package com.bytesize.entities;
 
+import java.util.Objects;
+
 public class Transaction {
+    private int transactionId;
+    private int amount;
+    private String size;
+    private String status;
+    private int productId;
+    private int buyerId;
+
+    public Transaction() {}
+
+    public Transaction(int transactionId, int amount, String size, String status, int productId, int buyerId) {
+        this.transactionId = transactionId;
+        this.amount = amount;
+        this.size = size;
+        this.status = status;
+        this.productId = productId;
+        this.buyerId = buyerId;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getBuyerId() {
+        return buyerId;
+    }
+
+    public void setBuyerId(int buyerId) {
+        this.buyerId = buyerId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return transactionId == that.transactionId && amount == that.amount && productId == that.productId && buyerId == that.buyerId && Objects.equals(size, that.size) && Objects.equals(status, that.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transactionId, amount, size, status, productId, buyerId);
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "transactionId=" + transactionId +
+                ", amount=" + amount +
+                ", size='" + size + '\'' +
+                ", status='" + status + '\'' +
+                ", productId=" + productId +
+                ", buyerId=" + buyerId +
+                '}';
+    }
 }
--- src/main/java/com/bytesize/entities/User.java
+++ src/main/java/com/bytesize/entities/User.java
@@ -1,4 +1,85 @@
 package com.bytesize.entities;
 
+import java.util.Objects;
+
 public class User {
+    private int userId;
+    private String username;
+    private String password;
+    private boolean isBuyer;
+    private boolean isSeller;
+
+    public User(){}
+
+    public User(int userId, String username, String password, boolean isBuyer, boolean isSeller){
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.isBuyer = isBuyer;
+        this.isSeller = isSeller;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setBuyer(boolean buyer) {
+        isBuyer = buyer;
+    }
+
+    public void setSeller(boolean seller) {
+        isSeller = seller;
+    }
+
+    public boolean isBuyer() {
+        return isBuyer;
+    }
+
+    public boolean isSeller() {
+        return isSeller;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userId == user.userId && isBuyer == user.isBuyer && isSeller == user.isSeller && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, username, password, isBuyer, isSeller);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", isBuyer=" + isBuyer +
+                ", isSeller=" + isSeller +
+                '}';
+    }
 }
